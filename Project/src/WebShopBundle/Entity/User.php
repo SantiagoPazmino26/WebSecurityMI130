ff40d52cc5216533f9926c9777c990b4a74d9e82
SantiagoPazmino26 <32416202+SantiagoPazmino26@users.noreply.github.com>
2018-06-14 19:28:55 +0200

    Update User.php
    Cascade added

--- Project/src/WebShopBundle/Entity/User.php
+++ Project/src/WebShopBundle/Entity/User.php
@@ -58,7 +58,7 @@ class User implements UserInterface
      *
      * @Assert\Count(min="1")
      *
-     * @ORM\ManyToMany(targetEntity="WebShopBundle\Entity\Role", inversedBy="users")
+     * @ORM\ManyToMany(targetEntity="WebShopBundle\Entity\Role", inversedBy="users", cascade={"persist"})
      * @ORM\JoinTable(name="users_roles")
      */
     private $roles;
